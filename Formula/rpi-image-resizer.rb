1e4cb281915b469fd354cc5898eb3518daa986ab
Andreas Heissenberger <andreas@heissenberger.at>
2025-12-02 04:49:19 +0100

    chore: update rpi-image-resizer to v0.0.21

--- Formula/rpi-image-resizer.rb
+++ Formula/rpi-image-resizer.rb
@@ -1,17 +1,16 @@
 class RpiImageResizer < Formula
   desc "Raspberry Pi image resize and partition adjuster (CLI)"
   homepage "https://github.com/aheissenberger/raspberry-image-resizer-docker"
-  version "0.0.20"
+  version "0.0.21"
 
   if Hardware::CPU.arm?
-    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.20/rpi-tool-darwin-arm64.tar.gz"
-    sha256 "c0bdfa4520aeeda5272bc79422b6d555ac2f2b0f76788e6a47beccdad2c0d801"
+    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.21/rpi-tool-darwin-arm64.tar.gz"
+    sha256 "50439fbaf8075f3c2bdee52e85534ca872096f91b4075ac17844e09deba4c302"
   else
-    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.20/rpi-tool-darwin-amd64.tar.gz"
+    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.21/rpi-tool-darwin-amd64.tar.gz"
     sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   end
 
-  depends_on "nbdkit"
   depends_on "docker" => :recommended
 
   def install
