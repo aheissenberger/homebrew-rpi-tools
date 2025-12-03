22279986555ae312d24f4f3245a75ee84e3f1d7b
Andreas Heissenberger <andreas@heissenberger.at>
2025-12-03 10:49:44 +0100

    chore: update rpi-image-resizer to v0.0.26

--- Formula/rpi-image-resizer.rb
+++ Formula/rpi-image-resizer.rb
@@ -1,20 +1,31 @@
 class RpiImageResizer < Formula
   desc "Raspberry Pi image resize and partition adjuster (CLI)"
   homepage "https://github.com/aheissenberger/raspberry-image-resizer-docker"
-  version "0.0.25"
+  version "0.0.26"
 
   if Hardware::CPU.arm?
-    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.25/rpi-tool-darwin-arm64.tar.gz"
-    sha256 "803cfe88f95a4741f25ec545ec8873cbe688e2772c0249adc59c5967d9a4d229"
+    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.26/rpi-tool-darwin-arm64.tar.gz"
+    sha256 "56ca034b2f769583b0025de0b96e4078759679243c100b2d4308a7cbb7621e99"
   else
-    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.25/rpi-tool-darwin-amd64.tar.gz"
+    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.26/rpi-tool-darwin-amd64.tar.gz"
     sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   end
 
-  depends_on "docker" => :recommended
-
   def install
     bin.install "rpi-tool"
+  end
+
+  def caveats
+    <<~EOS
+      This tool requires either Docker Desktop or a Docker-compatible container runtime.
+      
+      Install Docker Desktop:
+        brew install --cask docker
+      
+      Or use an existing Docker installation (including Docker Desktop, Colima, or Rancher Desktop).
+      Verify Docker is available:
+        docker --version
+    EOS
   end
 
   test do
